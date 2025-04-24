cbfaac90a6571a545c1efea9a0530c8141d71159
FlexLuthORF <zmvanwinkle@gmail.com>
2025-04-24 14:46:11 -0400

    added multiple loci support for clone_stats.R

--- python/clone_stats.R
+++ python/clone_stats.R
@@ -11,6 +11,7 @@ parse_args <- function() {
   list(input_filename=input_filename, locus=locus, prefix=prefix)
 }
 
+# Parse the arguments
 args     <- parse_args()
 dataFull <- airr::read_rearrangement(args$input_filename)
 lociToProcess <- if (args$locus=="TR") c("TRA","TRB","TRD","TRG") else args$locus
@@ -21,15 +22,18 @@ for (locus in lociToProcess) {
   if (nrow(data)==0) { message("  No data for ", locus, ", skipping."); next }
   tryCatch({
     if (locus=="IGH") {
+      # split by isotype. calculate abundance by sequence.
       data$isotype=substr(data$c_call,1,4)
       clones=countClones(data,group="isotype",clone="clone_id")
       acurve=estimateAbundance(data[!is.na(data$isotype),],group="isotype",ci=0.95,nboot=100,clone="clone_id")
       dcurve=alphaDiversity(acurve,min_q=0,max_q=4,step_q=0.1,ci=0.95)
     } else if (locus=="IGK"||locus=="IGL") {
+      # calculate abundance by sequence
       clones=countClones(data,clone="clone_id")
       acurve=estimateAbundance(data,ci=0.95,nboot=100,clone="clone_id")
       dcurve=alphaDiversity(acurve,min_q=0,max_q=4,step_q=0.1,ci=0.95,group=NULL)
     } else {
+      # calculate abundance by DUPCOUNT
       print("counting clones")
       clones=countClones(data,clone="clone_id",copy="duplicate_count")
       print("estimating abundance")
@@ -38,6 +42,7 @@ for (locus in lociToProcess) {
       dcurve=alphaDiversity(acurve,min_q=0,max_q=4,step_q=0.1,ci=0.95,group=NULL)
       print("finished calculation")
     }
+    
     write.csv(clones,paste(args$prefix,locus,"clone_freqs.csv",sep="_"),row.names=FALSE)
     write.csv(dcurve@diversity,paste(args$prefix,locus,"clone_diversity.csv",sep="_"),row.names=FALSE)
     png(paste(args$prefix,locus,"clone_abundance_plot.png",sep="_"));plot(acurve);dev.off()
