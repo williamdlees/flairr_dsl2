b5cf6bf85e2fba77929679ce0035b3aa1281d9fa
FlexLuthORF <zmvanwinkle@gmail.com>
2025-04-24 14:42:14 -0400

    added multiple loci support for clone_stats.R

--- python/clone_stats.R
+++ python/clone_stats.R
@@ -1,62 +1,48 @@
+#!/usr/bin/env Rscript
 library(alakazam)
 library(airr)
 
-# Function to parse command-line arguments
 parse_args <- function() {
-  args <- commandArgs(trailingOnly = TRUE)
-  
-  if (length(args) != 3) {
-    stop("Three arguments are required: input_filename, locus, output_prefix")
-  }
-  
-  input_filename <- args[1]
-  locus <- args[2]
-  prefix <- args[3]
-  
-  valid_loci <- c("IGH", "IGK", "IGL", "TRA", "TRB", "TRD", "TRG")
-  if (!(locus %in% valid_loci)) {
-    stop("Locus must be one of IGH, IGK, IGL, TRA, TRB, TRD, TRG")
-  }
-  
-  list(input_filename = input_filename, locus = locus, prefix=prefix)
+  args <- commandArgs(trailingOnly=TRUE)
+  if (length(args)!=3) stop("Three arguments are required: input_filename, locus, output_prefix")
+  input_filename <- args[1]; locus <- args[2]; prefix <- args[3]
+  valid_loci <- c("IGH","IGK","IGL","TRA","TRB","TRD","TRG","TR")
+  if (!(locus %in% valid_loci)) stop("Locus must be one of IGH, IGK, IGL, TRA, TRB, TRD, TRG or TR")
+  list(input_filename=input_filename, locus=locus, prefix=prefix)
 }
 
-# Parse the arguments
-args <- parse_args()
+args     <- parse_args()
+dataFull <- airr::read_rearrangement(args$input_filename)
+lociToProcess <- if (args$locus=="TR") c("TRA","TRB","TRD","TRG") else args$locus
 
-data = airr::read_rearrangement(args$input_filename)
-
-if (args$locus == 'IGH') {
-  # split by isotype. calculate abundance by sequence.
-  data[['isotype']]=substr(data[['c_call']], start=1, stop=4)
-  clones = countClones(data, group="isotype", clone="clone_id")
-  acurve = estimateAbundance(data[!is.na(data$isotype),], group="isotype", ci=0.95, nboot=100, clone="clone_id")
-  dcurve = alphaDiversity(acurve, min_q = 0, max_q = 4, step_q = 0.1, ci = 0.95)
-} else if (args$locus == 'IGK' || args$locus == 'IGL') {
-  # calculate abundance by sequence
-  clones = countClones(data, clone="clone_id")
-  acurve = estimateAbundance(data, ci=0.95, nboot=100, clone="clone_id")
-  dcurve = alphaDiversity(acurve, min_q = 0, max_q = 4, step_q = 0.1, ci = 0.95)
-} else {
-  # calculate abundance by DUPCOUNT
-  print("counting clones")
-  clones = countClones(data, clone="clone_id", copy="duplicate_count")
-  print("estimating abundance")
-  acurve = estimateAbundance(data, ci=0.95, nboot=100, clone="clone_id", copy="duplicate_count")
-  print("calculating diversity")
-  dcurve = alphaDiversity(acurve, min_q = 0, max_q = 4, step_q = 0.1, ci = 0.95)
-  print("finished calculation")
+for (locus in lociToProcess) {
+  message("Processing ", locus, " ...")
+  data <- dataFull[dataFull$locus==locus,]
+  if (nrow(data)==0) { message("  No data for ", locus, ", skipping."); next }
+  tryCatch({
+    if (locus=="IGH") {
+      data$isotype=substr(data$c_call,1,4)
+      clones=countClones(data,group="isotype",clone="clone_id")
+      acurve=estimateAbundance(data[!is.na(data$isotype),],group="isotype",ci=0.95,nboot=100,clone="clone_id")
+      dcurve=alphaDiversity(acurve,min_q=0,max_q=4,step_q=0.1,ci=0.95)
+    } else if (locus=="IGK"||locus=="IGL") {
+      clones=countClones(data,clone="clone_id")
+      acurve=estimateAbundance(data,ci=0.95,nboot=100,clone="clone_id")
+      dcurve=alphaDiversity(acurve,min_q=0,max_q=4,step_q=0.1,ci=0.95,group=NULL)
+    } else {
+      print("counting clones")
+      clones=countClones(data,clone="clone_id",copy="duplicate_count")
+      print("estimating abundance")
+      acurve=estimateAbundance(data,ci=0.95,nboot=100,clone="clone_id",copy="duplicate_count")
+      print("calculating diversity")
+      dcurve=alphaDiversity(acurve,min_q=0,max_q=4,step_q=0.1,ci=0.95,group=NULL)
+      print("finished calculation")
+    }
+    write.csv(clones,paste(args$prefix,locus,"clone_freqs.csv",sep="_"),row.names=FALSE)
+    write.csv(dcurve@diversity,paste(args$prefix,locus,"clone_diversity.csv",sep="_"),row.names=FALSE)
+    png(paste(args$prefix,locus,"clone_abundance_plot.png",sep="_"));plot(acurve);dev.off()
+    png(paste(args$prefix,locus,"clone_diversity_plot.png",sep="_"));plot(dcurve);dev.off()
+  }, error=function(e){
+    message("  ERROR processing ", locus, ": ", e$message, " — skipping.")
+  })
 }
-
-write.csv(clones, paste(args$prefix, 'clone_freqs.csv', sep='_'), row.names=FALSE)
-write.csv(dcurve@diversity, paste(args$prefix, 'clone_diversity.csv', sep='_'), row.names = FALSE)
-
-png(paste(args$prefix, 'clone_abundance_plot.png', sep='_'))
-plot(acurve)
-
-dev.off()
-png(paste(args$prefix, 'clone_diversity_plot.png', sep='_'))
-plot(dcurve)
-
-dev.off()
-
