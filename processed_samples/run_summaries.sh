dde0752ae715d9c044ce3880b99fbdad680eb0f9
William Lees <william@lees.org.uk>
2025-08-12 12:10:14 +0100

    relocate toml file to preprocess

--- processed_samples/run_summaries.sh
+++ processed_samples/run_summaries.sh
@@ -5,10 +5,10 @@ cd /data
 
 # Run each summary-generating script with absolute paths
 echo "🔄 Running pipeline_counts.py..."
-python3 /nf_root/python/pipeline_counts.py /nf_root/flairr_logs.toml /data/results/pipeline_counts.csv
+python3 /nf_root/python/pipeline_counts.py /nf_root/preprocess/flairr_logs.toml /data/results/pipeline_counts.csv
 
 echo "🔄 Running barcode_alignments.py..."
-python3 /nf_root/python/barcode_alignments.py /nf_root/flairr_logs.toml /data/results/barcode_alignments.html
+python3 /nf_root/python/barcode_alignments.py /nf_root/preprocess/flairr_logs.toml /data/results/barcode_alignments.html
 
 echo "🔄 Running project_allele_table.py..."
 python3 /nf_root/python/project_allele_table.py flairr_test /data/results /data/results/flairr_allele_usage_table.csv
