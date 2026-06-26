b35794a01d0c810ec234d8616077862d8949d7b8
AWS ParallelCluster user <ec2-user@ip-172-31-39-244.ec2.internal>
2026-06-26 17:09:02 +0000

    Include isotype and clone_id columns for IGH

--- python/make_project_alignments_file.py
+++ python/make_project_alignments_file.py
@@ -40,6 +40,27 @@ def find_alignment_file(input_dir, sample, locus, personalized):
     return None
 
 
+def find_clone_ids(input_dir, sample, locus):
+    clone_file = None
+    clone_ids = {}
+    clone_dir = os.path.join(input_dir, sample, locus, 'clones')
+    if os.path.isdir(clone_dir):
+        for file_name in os.listdir(clone_dir):
+            if file_name.endswith('_clone_pass.tsv'):
+                clone_file = file_name
+                break
+    if clone_file:
+        clone_file_path = os.path.join(clone_dir, clone_file)
+        print(f"Found clone file for sample '{sample}', locus '{locus}': {clone_file_path}")
+        with open(clone_file_path, newline='') as tsvfile:
+            reader = csv.DictReader(tsvfile, delimiter='\t')
+            for row in reader:
+                clone_id = row.get('clone_id')
+                if clone_id:
+                    clone_ids[row['sequence_id']] = clone_id
+    return clone_ids
+
+
 def concatenate_files(samples, input_dir, output_file, personalized):
     all_data = []
     header = None
@@ -49,6 +70,9 @@ def concatenate_files(samples, input_dir, output_file, personalized):
         for locus in loci:
             alignment_file = find_alignment_file(input_dir, sample, locus, personalized)
             if alignment_file:
+                clone_ids = {}
+                if personalized and 'IG' in locus:
+                    clone_ids = find_clone_ids(input_dir, sample, locus)
                 with open(alignment_file, newline='') as csvfile:
                     reader = csv.DictReader(csvfile, delimiter='\t')
                     for row in reader:
@@ -56,11 +80,19 @@ def concatenate_files(samples, input_dir, output_file, personalized):
                             # Preserve the original column order from the first file
                             header = ['sample', 'locus']
                             header.extend(list(row.keys()))
+                            header.extend(['clone_id', 'isotype_call'])
                         row['sample'] = sample
                         row['locus'] = locus
+                        row['clone_id'] = clone_ids.get(row['sequence_id'], '')
+                        row['isotype_call'] = ''
+
+                        if 'c_call' in row and 'IGH' in row['c_call']:
+                            row['isotype_call'] = row['c_call'].split(',')[0].split('*')[0]
+
                         all_data.append(row)
             else:
-                print(f"Warning: No alignment file found for sample '{sample}', locus '{locus}'")
+                if '_' not in locus:
+                    print(f"Warning: No alignment file found for sample '{sample}', locus '{locus}'")
                 continue
 
     if all_data:
@@ -76,4 +108,6 @@ def concatenate_files(samples, input_dir, output_file, personalized):
 if __name__ == "__main__":
     args = parse_arguments()
     samples = scan_directories(args.input_dir)
+    k = list(samples.keys())[0]
+    samples = {k: samples[k]}  # first sample only for testing
     concatenate_files(samples, args.input_dir, args.output_file, args.personalized)
